44bb4a6c58413b12c2b8a6952542771d46b7ee69
Yanagawa <fred642120@gmail.com>
2024-02-21 17:57:57 +0800

    Homework4-change2

--- src/hw1/Homework4.java
+++ src/hw1/Homework4.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class Homework4 {
 	public static void main(String[] args) {
-		int[] a = { 29, 100, 39, 41, 50, 8, 66, 77, 95, 15 };
-		int b = 0;	//總和
-		int d = 0;	//陣列長
-		for (int c = 0; c < a.length; c++) {
-			b = b + a[c];
-			d++;
-			}
-		int e = b / d;	//平均值
-		System.out.println("平均值為" + e);
-		System.out.println("大於平均值的有:");
-		for (int c = 0; c < a.length; c++) {
-			if(e<a[c]) {
-				System.out.print("a["+c+"]"+"\t");
-				System.out.println(a[c]);
-			}
-		}
+//		int[] a = { 29, 100, 39, 41, 50, 8, 66, 77, 95, 15 };
+//		int b = 0;	//總和
+//		int d = 0;	//陣列長
+//		for (int c = 0; c < a.length; c++) {
+//			b = b + a[c];
+//			d++;
+//			}
+//		int e = b / d;	//平均值
+//		System.out.println("平均值為" + e);
+//		System.out.println("大於平均值的有:");
+//		for (int c = 0; c < a.length; c++) {
+//			if(e<a[c]) {
+//				System.out.print("a["+c+"]"+"\t");
+//				System.out.println(a[c]);
+//			}
+//		}
 //================================================================================================
 //		Scanner sc = new Scanner(System.in);
-//		System.out.println("請輸入一字串：");
+//		System.out.println("請輸入一串文字：");
 //		String a=sc.nextLine();
 //		int b= a.length();	//字串長
 //		
@@ -82,8 +82,7 @@ public class Homework4 {
 //		int g = 0; // 總天數
 //
 //		if (month <= 12) {
-//			// 閏年的計算
-//			if (ad % 4 == 0) {
+//			if (ad % 4 == 0) { // 閏年的計算
 //				if (date <= everyMonth[month]) {
 //					for (int h = 0; h < month; h++) {
 //						g = g + everyMonth[h];
@@ -112,58 +111,51 @@ public class Homework4 {
 //			System.out.println("月份輸入錯誤");
 //		}
 //=================================================================================================
-//		int a[][] = { { 10, 35, 40, 100, 90, 85, 75, 70 }, { 37, 75, 77, 89, 64, 75, 70, 95 },
-//				{ 100, 70, 79, 90, 75, 70, 79, 90 }, { 77, 95, 70, 87, 60, 75, 85, 89 },
-//				{ 98, 70, 89, 90, 75, 90, 89, 90 }, { 90, 80, 100, 75, 50, 20, 99, 75 } };
-//		int num1 = 0;
-//		int num2 = 0;
-//		int num3 = 0;
-//		int num4 = 0;
-//		int num5 = 0;
-//		int num6 = 0;
-//		int num7 = 0;
-//		int num8 = 0;
-//
-//		// 找出各個陣列裡最大的值的位置
-//		for (int b = 0; b < a.length; b++) {
-//			int maxNum = 0;
-//			int d = 0;
-//			for (int c = 0; c < a[b].length; c++) {
-//				if (maxNum < a[b][c]) {
-//					maxNum = a[b][c];
-//					d = c;
-//				}
-//			}
-//
-//			// 將各個位置出現的次數計算
-//			switch (d) {
-//			case 0:
-//				++num1;
-//				break;
-//			case 1:
-//				++num2;
-//				break;
-//			case 2:
-//				++num3;
-//				break;
-//			case 3:
-//				++num4;
-//				break;
-//			case 4:
-//				++num5;
-//				break;
-//			case 5:
-//				++num6;
-//				break;
-//			case 6:
-//				++num7;
-//				break;
-//			case 7:
-//				++num8;
-//				break;
-//			}
-//		}
-//		System.out.println("各個考最高分次數:1號" + num1 + "次," + "2號" + num2 + "次," + "3號" + num3 + "次," + "4號" + num4 + "次,"
-//				+ "5號" + num5 + "次," + "6號" + num6 + "次," + "7號" + num7 + "次," + "8號" + num8 + "次");
+		int a[][] = { { 10, 35, 40, 100, 90, 85, 75, 70 }, { 37, 75, 77, 89, 64, 75, 70, 95 },
+				{ 100, 70, 79, 90, 75, 70, 79, 90 }, { 77, 95, 70, 87, 60, 75, 85, 89 },
+				{ 98, 70, 89, 90, 75, 90, 89, 90 }, { 90, 80, 100, 75, 50, 20, 99, 75 } };
+		int num1 = 0, num2 = 0, num3 = 0, num4 = 0, num5 = 0, num6 = 0, num7 = 0, num8 = 0;
+
+		// 找出各個陣列裡最大的值的位置
+		for (int b = 0; b < a.length; b++) {
+			int maxNum = 0;
+			int d = 0;
+			for (int c = 0; c < a[b].length; c++) {
+				if (maxNum < a[b][c]) {
+					maxNum = a[b][c];
+					d = c;
+				}
+			}
+
+			// 將各個位置出現的次數計算
+			switch (d) {
+			case 0:
+				++num1;
+				break;
+			case 1:
+				++num2;
+				break;
+			case 2:
+				++num3;
+				break;
+			case 3:
+				++num4;
+				break;
+			case 4:
+				++num5;
+				break;
+			case 5:
+				++num6;
+				break;
+			case 6:
+				++num7;
+				break;
+			case 7:
+				++num8;
+				break;
+			}
+		}
+		System.out.println("各個考最高分次數:1號" + num1 + "次," + "2號" + num2 + "次," + "3號" + num3 + "次," + "4號" + num4 + "次,"
+				+ "5號" + num5 + "次," + "6號" + num6 + "次," + "7號" + num7 + "次," + "8號" + num8 + "次");
 	}
 }
