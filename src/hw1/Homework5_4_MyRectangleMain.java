11d0f7e7fb8af6431793a1d4ee3635515a86e0d5
Yanagawa <fred642120@gmail.com>
2024-02-25 09:04:34 +0800

    Homework5_5 simplify and input validation

--- src/hw1/Homework5_4_MyRectangleMain.java
+++ src/hw1/Homework5_4_MyRectangleMain.java
@@ -6,6 +6,7 @@ public class Homework5_4_MyRectangleMain {
 		a.setWidth(10.0);
 		a.setDepth(20.0);
 		System.out.println(a.getArea());
+		
 		Homework5_4_MyRectangle b=new Homework5_4_MyRectangle(10.0,20.0);
 		System.out.println(b.getArea());
 	}
--- src/hw1/Homework5_5.java
+++ src/hw1/Homework5_5.java
@@ -1,36 +1,42 @@
 package hw1;
 
+import java.util.Scanner;
+
 public class Homework5_5 {
 	public static String getAuthCode() {
-		int[] number = new int[8];
 		String verify = "";
-
-		// 生成能轉成字母及1~9的整數
-		for (int i = 0; i < number.length; i++) {
+		for (int i = 0; i < 8; i++) {
 			fall: for (;;) {
 				int a = (int) (Math.random() * 122) + 1;
-				if (a < 10 || (a > 64 && a < 91) || (a > 96 && a < 123)) {
-					number[i] = a;
-					break;
-				} else {
-					continue fall; //沒拿到要求的數字就退回重拿
-				}
-			}
-		}
 
-		// 將生成的數字轉成字母(除1~9),並相接成字串
-		for (int i = 0; i < number.length; i++) {
-			if (number[i] < 10) {
-				verify = verify + number[i];
-			} else {
-				char change = (char) number[i];
-				verify = verify + change;
+				// 保留1~9的數字,將65~90轉換成A~Z,97~122轉換成a~z
+				if (a < 10 || (a > 64 && a < 91) || (a > 96 && a < 123)) {
+					if (a < 10) { // 將數字(除了1~9之外)轉成字元,並串接成字串
+						verify = verify + a;
+						break; // 拿到符合規則的東西後跳出無限迴圈
+					} else {
+						char change = (char) a;
+						verify = verify + change;
+						break; // 拿到符合規則的東西後跳出無限迴圈
+					}
+				} else {
+					continue fall; // 沒拿到要求的數字就退回重拿
+				}
 			}
 		}
 		return verify;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(getAuthCode());
+		String get = getAuthCode();
+		System.out.println(get);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("請輸入驗證碼");
+		String reply = sc.nextLine();
+		if (reply.equals(get)) {
+			System.out.println("輸入正確");
+		} else {
+			System.out.println("輸入錯誤");
+		}
 	}
 }
