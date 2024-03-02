a73360b0b6c1ea20e3459845d6dd4f1c1d777b19
Yanagawa <fred642120@gmail.com>
2024-03-02 12:25:35 +0800

    HomeWork6_Alter-Validation of incorrect input format

--- src/Homework6/CalTest.java
+++ src/Homework6/CalTest.java
@@ -1,24 +1,41 @@
 package Homework6;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalTest {
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Calculator myCalculator = new Calculator();
-		Scanner sc = new Scanner(System.in);
 		re: for (;;) {
-			System.out.println("請輸入x的值");
-			String x = sc.nextLine();
-			System.out.println("請輸入y的值");
-			String y = sc.nextLine();
 			try {
-				myCalculator.setCalculator(x, y);
-				System.out.println(x + "的" + y + "次方等於" + myCalculator.getCalculator());
-				sc.close();
-				break;
-			} catch (Exception e) {
-				System.out.println(e.toString());
-				System.out.println("請從新輸入");
+				Scanner sc = new Scanner(System.in);
+				System.out.println("請輸入x的值");
+				int x = sc.nextInt();
+				re2: for (;;) {
+					try {
+						Scanner sc2 = new Scanner(System.in);
+						System.out.println("請輸入y的值");
+						int y = sc2.nextInt();
+						try {
+							myCalculator.setCalculator(x, y);
+							System.out.println(x + "的" + y + "次方等於" + myCalculator.getCalculator());
+							sc2.close();
+							sc.close();
+							break re;
+						} catch (Exception e) {
+							System.out.println(e.toString());
+							System.out.println("請從新輸入");
+							continue re;
+						}
+					} catch (InputMismatchException a) {
+						System.out.println("輸入格式不正確");
+						System.out.println("請從新輸入y的值");
+						continue re2;
+					}
+				}
+			} catch (InputMismatchException b) {
+				System.out.println("輸入格式不正確");
+				System.out.println("請從新輸入x的值");
 				continue re;
 			}
 		}
--- src/Homework6/Calculator.java
+++ src/Homework6/Calculator.java
@@ -1,35 +1,29 @@
 package Homework6;
 
 public class Calculator {
-	private int a;
-	private int b;
+	private int x;
+	private int y;
 
 	public Calculator() {
 	}
 
-	public Calculator(String a, String b) throws CalException {
-		setCalculator(a, b);
+	public Calculator(int x, int y) throws CalException {
+		setCalculator(x, y);
 	}
 
-	void setCalculator(String a, String b) throws CalException {
-		try {
-			int x = Integer.parseInt(a);
-			int y = Integer.parseInt(b);
-			if (x == 0 && y == 0) {
-				throw new CalException("0的0次方沒有意義!");
-			} else if (y < 0) {
-				throw new CalException("次方為負值,結果回傳不為整數!");
-			} else {
-				this.a = x;
-				this.b = y;
-			}
-		} catch (NumberFormatException e) {
-			throw new CalException("輸入格式不正確,請輸入整數");
+	void setCalculator(int x, int y) throws CalException {
+		if (x == 0 && y == 0) {
+			throw new CalException("0的0次方沒有意義!");
+		} else if (y < 0) {
+			throw new CalException("次方為負值,結果回傳不為整數!");
+		} else {
+			this.x = x;
+			this.y = y;
 		}
 	}
 
 	double getCalculator() {
 
-		return Math.pow(a, b);
+		return Math.pow(x, y);
 	}
 }
